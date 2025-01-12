04bf4cfb255f198822198da29b16abf35f263ad1
JeffersonBenetti <jefferson.benetti@hotmail.com>
2025-01-12 04:12:59 -0300

    feat: Starting javascript module. - classes 01 and 02.

---
+++ Front-End-Course/Módulo 05/JAVASCRIPT/02 - LogicaDeProgramação.js
@@ -0,0 +1,5 @@
+// - Lógica de programação:
+
+// - Em linhas gerais, lógica de programação é todo conjunto de regras e conceitos que precisamos aplicar para criar códigos que serão interpretados e executados por um computador.
+
+// - Um algoritmo é uma sequência de passos que visa solucionar um problema específico. Eles são utilizados em diversas áreas do conhecimento, como matemática, engenharia, física e, claro, na programação. Em programação, um algoritmo é uma sequência de instruções que pode ser executada por um computador.
