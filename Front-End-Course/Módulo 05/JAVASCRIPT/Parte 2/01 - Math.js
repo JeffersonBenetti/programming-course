adb24f2d6ab572fba9ef991e16057e7b2af56c8e
jeffersonBenetti <jefferson.benetti@hotmail.com>
2025-01-16 16:37:15 -0300

    feat: Starting part 2 of the javascript module plus lesson 1.

---
+++ Front-End-Course/Módulo 05/JAVASCRIPT/Parte 2/01 - Math.js
@@ -0,0 +1,35 @@
+/*
+    - Math: Matemática.
+
+    - Pow(2,2) / Potência / 2² = 4
+    - SQRT(25,2) / Raiz / /25
+    - Pi -> 3,14.
+    - Ceil / teto -> Arredondar para cima.
+    - Flor / chão -> Arredondar para baixo.
+
+    - Random() / Número aleatório entre 0 e 1.
+*/
+
+//Math.pow
+const result = Math.pow(5,3)
+console.log(result);
+
+//Math.sqrt
+const result1 = Math.sqrt(25,2)
+console.log(result1);
+
+//Math.pi
+const result2 = Math.PI
+console.log(result2);
+
+//Math.ceil
+const result3 = Math.ceil(3.14)
+console.log(result3);
+
+//Math.flor
+const result4 = Math.floor(3.14)
+console.log(result4);
+
+//Math.random
+const result5 = Math.random()
+console.log(result4);
