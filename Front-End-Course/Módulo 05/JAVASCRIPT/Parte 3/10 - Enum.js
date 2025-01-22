4bdacaaffce5823207b3f4b16e0d94bfe396d014
jeffersonBenetti <jefferson.benetti@hotmail.com>
2025-01-22 17:04:44 -0300

    feat: Completion of part 3 of the javascript module. - project completed. - lesson 10.

--- Front-End-Course/Módulo 05/JAVASCRIPT/Parte 3/10 - Enum.js
+++ Front-End-Course/Módulo 05/JAVASCRIPT/Parte 3/10 - Enum.js
@@ -1,3 +1,15 @@
+/*
+    - Enum:
+
+    - O formato utilizado para fazer o ENUM é:
+
+        - GAME_OPTIONS -> Snake Case
+                
+                não o
+        
+        - gameOptions -> Camel Case.
+*/
+
 const result = document.querySelector('.result')
 const yourScore = document.querySelector('#your-score')
 const machineScore = document.querySelector('#machine-score')
@@ -5,12 +17,18 @@ const machineScore = document.querySelector('#machine-score')
 let pointForHuman = 0
 let pointForMachine = 0
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
 
     return choices[randomNumber]
@@ -21,9 +39,9 @@ const playTheGame = (human, machine) => {
         result.innerHTML = 'Deu Empate!'
     }
     else if (
-        (human == 'paper' && machine == 'rock') ||
-        (human == 'rock' && machine == 'scissors') ||
-        (human == 'scissors' && machine == 'paper')) {
+        (human == GAME_OPTIONS.PAPER && machine == GAME_OPTIONS.ROCK) ||
+        (human == GAME_OPTIONS.ROCK && machine == GAME_OPTIONS.SCISSORS) ||
+        (human == GAME_OPTIONS.SCISSORS && machine == GAME_OPTIONS.PAPER)) {
         pointForHuman++
         yourScore.innerHTML = pointForHuman
         result.innerHTML = 'Você GANHOUUU!'
