15110512f466c054de917c044226351806294fbd
jeffersonBenetti <jefferson.benetti@hotmail.com>
2025-01-24 17:48:38 -0300

    feat: Burger project javascript module part 4.

---
+++ Front-End-Course/Módulo 05/JAVASCRIPT/Parte 4/Project-Burgers/products.js
@@ -0,0 +1,8 @@
+const menuOptions = [
+    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg', alt: 'X-Salada' },
+    { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png', alt: 'X-Bacon' },
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png', alt: 'X-Bacon-Egg' },
+    { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png', alt: 'Monstruoso' },
+    { name: 'Big Vegano', price: 55, vegan: true, src: './img/xvegan.png', alt: 'Big-Vegano' },
+    { name: 'X-Vegan', price: 45, vegan: true, src: './img/monstruoso-vegan.png', alt: 'X-Vegan' },
+    ]
