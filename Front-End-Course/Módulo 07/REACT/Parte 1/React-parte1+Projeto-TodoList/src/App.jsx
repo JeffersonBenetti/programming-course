7ba3b06802fe6d9abd4bba8e64a756b4d9f2e85d
jeffersonBenetti <jefferson.benetti@hotmail.com>
2025-02-03 14:43:59 -0300

    feat: React module part 1. Class 18. - Adding new item to the list. - Spread Operator.

--- Front-End-Course/Módulo 07/REACT/Parte 1/React-parte1+Projeto-TodoList/src/App.jsx
+++ Front-End-Course/Módulo 07/REACT/Parte 1/React-parte1+Projeto-TodoList/src/App.jsx
@@ -9,15 +9,18 @@ function App() {
 
   // Aula 13 - Key e 14 - Bibliotéca "uuid".
   const [list, setList] = useState([{id: uuid(), task: 'Terminar o módulo React parte 1'}])
+  const [task, setTask] = useState('')
 
   // Aula 11 - Eventos.
   function inputMudou(event){
-    setList({id: uuid(), task: event.target.value})
+    setTask(event.target.value)
   }
 
   // Aula 11 - Eventos.
   function cliqueiNoBotão(){
-    console.log('Botão clicado')
+    // Nota que quando a CHAVE tem o mesmo nome do VALOR, não precisa colocar os dois(Aulas 15, 16 e 17).
+    // Usamos o spread operator para sempre estar add o item existente ao novo array(Aula 18).
+    setList([...list, {id: uuid(), task}])
   }
 
   return (
