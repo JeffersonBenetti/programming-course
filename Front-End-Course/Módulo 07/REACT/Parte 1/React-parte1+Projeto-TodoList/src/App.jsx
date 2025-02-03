1674189dcb76a14a66674eb80e20436a7750c586
jeffersonBenetti <jefferson.benetti@hotmail.com>
2025-02-03 15:07:00 -0300

    feat: React module part 1, classes 19 and 20. - Reactionless CSS. - Styled components.

--- Front-End-Course/Módulo 07/REACT/Parte 1/React-parte1+Projeto-TodoList/src/App.jsx
+++ Front-End-Course/Módulo 07/REACT/Parte 1/React-parte1+Projeto-TodoList/src/App.jsx
@@ -2,41 +2,43 @@ import { v4 as uuid } from 'uuid';
 
 import React, { useState } from 'react';
 
+import { Container } from './styles.js'
+
 function App() {
 
   // Aula 12 - Map.
   // const list = ["Esquentar comida", "Terminar o módulo React parte 1"]
 
   // Aula 13 - Key e 14 - Bibliotéca "uuid".
-  const [list, setList] = useState([{id: uuid(), task: 'Terminar o módulo React parte 1'}])
+  const [list, setList] = useState([{ id: uuid(), task: 'Terminar o módulo React parte 1' }])
   const [task, setTask] = useState('')
 
   // Aula 11 - Eventos.
-  function inputMudou(event){
+  function inputMudou(event) {
     setTask(event.target.value)
   }
 
   // Aula 11 - Eventos.
-  function cliqueiNoBotão(){
+  function cliqueiNoBotão() {
     // Nota que quando a CHAVE tem o mesmo nome do VALOR, não precisa colocar os dois(Aulas 15, 16 e 17).
     // Usamos o spread operator para sempre estar add o item existente ao novo array(Aula 18).
-    setList([...list, {id: uuid(), task}])
+    setList([...list, { id: uuid(), task }])
   }
 
   return (
-    <div>
+    <Container>
       <input onChange={inputMudou} placeholder="O que tenho para fazer.." />
       <button onClick={cliqueiNoBotão}>Adicionar</button>
 
       <ul>
         {
           // Aula 12 - Map.
-          list.map( item => (
+          list.map(item => (
             <li key={item.id}>{item.task}</li>
           ))
         }
       </ul>
-    </div>
+    </Container>
   )
 }
 
