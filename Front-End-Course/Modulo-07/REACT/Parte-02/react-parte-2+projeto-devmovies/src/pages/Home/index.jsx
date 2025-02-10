059d09b26dca3e7d0986b66d7212c3c6a6f143de
jeffersonBenetti <jefferson.benetti@hotmail.com>
2025-02-10 18:01:23 -0300

    feat: React module part 2. installing Axios and styled-components. - Starting home styling.

--- Front-End-Course/Modulo-07/REACT/Parte-02/react-parte-2+projeto-devmovies/src/pages/Home/index.jsx
+++ Front-End-Course/Modulo-07/REACT/Parte-02/react-parte-2+projeto-devmovies/src/pages/Home/index.jsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
+
+import api from '../../services/api.js'
+import { Background } from './styles.js' 
+
 function Home() {
+  const [movie, setMovie] = useState()
+
+  async function getMovies(){
+    const data = await api.get('/movie/popular')
+
+    // setMovie(data.data.results[0])
+    console.log(data)
+  }
+
+  getMovies()
 
   return (
-    <div>
+    <Background $img={'https://image.tmdb.org/t/p/original/v9Du2HC3hlknAvGlWhquRbeifwW.jpg'}>
       <h1>Home</h1>
       <p>Essa é a Home</p>
-    </div>
+    </Background>
   );
 }
 
